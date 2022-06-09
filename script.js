cd5c8df28b9f8f8ce71e2946d227a1f9d385c914
irisdordio <dordioiris@gmail.com>
2022-06-09 16:50:39 +0100

    mobile menu

--- script.js
+++ script.js
@@ -41,3 +41,12 @@ document.querySelectorAll(".table-sortable th").forEach(headerCell => {
         sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
     });
 });
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
